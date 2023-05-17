255bf2669a49b3633304213012c794f571382e67
alonrabinowitz <62042858+alonrabinowitz@users.noreply.github.com>
2023-05-17 12:33:08 -0700

    change to normal graphics project

---
+++ Main.java
@@ -0,0 +1,24 @@
+import processing.core.*;
+
+public class Main extends PApplet {
+
+
+	public void settings() {
+		size(640, 550);
+	}
+
+	public void setup() {
+
+	}
+
+	@Override
+	public void draw() {
+
+	}
+
+
+	// main method to launch this Processing sketch from computer
+	public static void main(String[] args) {
+		PApplet.main(new String[] { "Main" });
+	}
+}

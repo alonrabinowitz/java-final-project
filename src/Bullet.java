79a87025bf2a37486387243171c925c5a0d6b068
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-05-19 12:27:49 -0700

    Minor edits.

--- src/Bullet.java
+++ src/Bullet.java
@@ -12,9 +12,6 @@ public class Bullet {
         this.width = 10;
         this.height = 10;
     }
-    public boolean isOffScreen(int screenWidth, int screenHeight) {
-        return this.x < -this.width || this.x > screenWidth || this.y < -this.height || this.y > screenHeight;
-    }
 
     public void act() {
         this.x += this.xSpeed;
@@ -24,5 +21,9 @@ public class Bullet {
     public void draw(Main main) {
         main.fill(255);
         main.ellipse(this.x, this.y, this.width, this.height);
+    }
+
+    public boolean isOffScreen(int screenWidth, int screenHeight) {
+        return this.x < -this.width || this.x > screenWidth || this.y < -this.height || this.y > screenHeight;
     }
 }

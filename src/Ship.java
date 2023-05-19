73f5d40c6b8eef9ae1631bf32a8e493e4f90ef5c
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-05-19 11:23:17 -0700

    Speed changes

--- src/Ship.java
+++ src/Ship.java
@@ -5,7 +5,7 @@ public class Ship {
     public Ship() {
         x = (int)(Math.random() * 2) * 700 - ((int)(Math.random() * 50) + 50);
         y = (int)(Math.random() * 450) + 50;
-        speed = (int)(Math.random() * 5) + 1;
+        speed = (int)(Math.random() * 3) + 3;
         width = 40;
         height = 20;
     }

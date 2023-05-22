118371286c6ec3c09053a63b0cedb50517d19d0e
alonrabinowitz <62042858+alonrabinowitz@users.noreply.github.com>
2023-05-22 12:54:24 -0700

    Add bullet counter

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 public class Main extends PApplet {
 	ArrayList<Bullet> bulletList = new ArrayList<>();
 	ArrayList<Ship> shipList = new ArrayList<>();
-	int shipSpawnCounter = 0;
 	PImage ship, turret, bullet, background;
+	int shipSpawnCounter, bulletsLeft;
 
 	public void settings() {
 		size(1280, 720);
@@ -19,6 +19,8 @@ public class Main extends PApplet {
 		turret = loadImage("../assets/turret_complete.png");
 		turret.resize(100, 100);
 		bullet = loadImage("../assets/bullet.png");
+		shipSpawnCounter = 0;
+		bulletsLeft = 10;
 	}
 
 	public void draw() {
@@ -46,15 +48,21 @@ public class Main extends PApplet {
 			Bullet bullet = bulletList.get(i);
 			if (bullet.act(this)) i--;
 		}
+
+		for(int i = 0; i < bulletsLeft; i++){
+			image(bullet, 10+20*i, 10, 10, 24);
+		}
 	}
 
 	public void mouseReleased() {
 		bulletList.add(new Bullet(640, 570, (float)(Math.PI/2), bullet));
+		bulletsLeft--;
 	}
 
 	public void keyReleased() {
 		if (key == ' ') {
 			bulletList.add(new Bullet(640, 570, (float)(Math.PI/2), bullet));
+			bulletsLeft--;
 		}
 	}
 

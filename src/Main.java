7c67ca113fb68e982be1c850c280e4901a42848c
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-05-22 14:54:37 -0700

    Minor edits

--- src/Main.java
+++ src/Main.java
@@ -31,7 +31,7 @@ public class Main extends PApplet {
 
 		//Randomly spawn ships
 		shipSpawnCounter += (int)(Math.random() * 10);
-		if (shipSpawnCounter > 90) {
+		if (shipSpawnCounter > 80) {
 			shipList.add(new Ship(shipImg));
 			shipSpawnCounter = 0;
 		}
@@ -49,7 +49,7 @@ public class Main extends PApplet {
 			if (bullet.act(this)) i--;
 		}
 
-		for(int i = 0; i < bulletsLeft; i++){
+		for (int i = 0; i < bulletsLeft; i++){
 			image(bulletImg, 10+20*i, 10, 16, 16);
 		}
 	}
@@ -61,7 +61,7 @@ public class Main extends PApplet {
 
 	public void keyReleased() {
 		if (key == ' ') {
-			if(bulletsLeft > 0) {
+			if (bulletsLeft > 0) {
 				bulletList.add(new Bullet(640, 570, 1, (float)(Math.PI/2), bulletImg));
 				bulletsLeft--;
 			}

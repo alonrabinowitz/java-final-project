315f81d8e534e39ca568baf177efad0d0cbea030
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-05-26 11:56:31 -0700

    Adding in three-screen logic and changed score to only write and save at the end of the game.

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 public class Main extends PApplet {
 	ArrayList<Bullet> bulletList = new ArrayList<>();
 	ArrayList<Ship> shipList = new ArrayList<>();
+	int gameState;
 	int shipSpawnCounter, bulletsLeft;
 	PImage shipImg, turretImg, bulletImg;
 	BG background;
@@ -17,6 +18,7 @@ public class Main extends PApplet {
 	}
 
 	public void setup() {
+		gameState = 0;
 		shipSpawnCounter = 0;
 		bulletsLeft = 10;
 		shipImg = loadImage("../assets/obj/alien_cropped.png");
@@ -28,50 +30,101 @@ public class Main extends PApplet {
 	}
 
 	public void draw() {
-		background(200,130,0);
-		fill(255);
-		rect(0, height - 50, width, 50);
-		background.draw(this);
-		image(turretImg, width/2 - 50, height - 150);
+		switch (gameState) {
+			case 0:
+				background(0);
+				fill(255);
+				textSize(50);
+				textAlign(CENTER);
+				text("Click to start", (float)width/2, (float)height/2);
+				break;
+			case 1:
+				background(200, 130, 0);
+				fill(255);
+				rect(0, height - 50, width, 50);
+				background.draw(this);
+				image(turretImg, width / 2 - 50, height - 150);
 
-		//Randomly spawn ships
-		shipSpawnCounter += (int)(Math.random() * 10);
-		if (shipSpawnCounter > 50) {
-			shipList.add(new Ship(shipImg));
-			shipSpawnCounter = 0;
-		}
+				//Randomly spawn ships
+				shipSpawnCounter += (int) (Math.random() * 10);
+				if (shipSpawnCounter > 50) {
+					shipList.add(new Ship(shipImg));
+					shipSpawnCounter = 0;
+				}
 
-		//Act and draw ships
-		for (int i = 0; i < shipList.size(); i++) {
-			Ship ship = shipList.get(i);
-			ship.act(this);
-			if (ship.act(this)) i--;
-		}
-		//Act and draw bullets
-		for (int i = 0; i < bulletList.size(); i++) {
-			Bullet bullet = bulletList.get(i);
-			if (bullet.act(this)) i--;
-		}
+				//Act and draw ships
+				for (int i = 0; i < shipList.size(); i++) {
+					Ship ship = shipList.get(i);
+					ship.act(this);
+					if (ship.act(this)) i--;
+				}
+				//Act and draw bullets
+				for (int i = 0; i < bulletList.size(); i++) {
+					Bullet bullet = bulletList.get(i);
+					if (bullet.act(this)) i--;
+				}
 
-		for (int i = 0; i < bulletsLeft; i++){
-			image(bulletImg, 17 + 30 * i, 17, 20, 20);
-		}
+				for (int i = 0; i < bulletsLeft; i++) {
+					image(bulletImg, 17 + 30 * i, 17, 20, 20);
+				}
 
-		score.draw(this);
+				score.draw(this);
+
+				if (bulletsLeft == 0 && bulletList.size() == 0) gameState = 2;
+
+				break;
+			case 2:
+				background(0);
+				fill(255);
+				textSize(50);
+				textAlign(CENTER);
+				text("Game Over", width / 2, height / 2);
+				textSize(30);
+				text("Click to restart", width / 2, height / 2 + 50);
+				break;
+		}
 	}
 
 	public void mouseReleased() {
-		if (bulletsLeft > 0) {
-			bulletsLeft--;
-			bulletList.add(new Bullet(640, 570, 1, (float)(Math.PI / 2), bulletImg));
+		switch (gameState) {
+			case 0:
+				gameState = 1;
+				break;
+			case 1:
+				if (bulletsLeft > 0) {
+					bulletsLeft--;
+					bulletList.add(new Bullet(640, 570, 1, (float)(Math.PI / 2), bulletImg));
+				}
+				break;
+			case 2:
+				gameState = 1;
+				shipList.clear();
+				bulletList.clear();
+				bulletsLeft = 10;
+				score.reset();
+				break;
 		}
 	}
 
 	public void keyReleased() {
 		if (key == ' ') {
-			if (bulletsLeft > 0) {
-				bulletList.add(new Bullet(640, 570, 1, (float)(Math.PI/2), bulletImg));
-				bulletsLeft--;
+			switch (gameState) {
+				case 0:
+					gameState = 1;
+					break;
+				case 1:
+					if (bulletsLeft > 0) {
+						bulletsLeft--;
+						bulletList.add(new Bullet(640, 570, 1, (float)(Math.PI / 2), bulletImg));
+					}
+					break;
+				case 2:
+					gameState = 1;
+					shipList.clear();
+					bulletList.clear();
+					bulletsLeft = 10;
+					score.reset();
+					break;
 			}
 		}
 	}
